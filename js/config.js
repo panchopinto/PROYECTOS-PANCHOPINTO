eee7ccdeb2b0e7e9b9638947df5b0734047dd638
panchopinto <69988761+panchopinto@users.noreply.github.com>
2025-09-03 15:00:18 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,16 @@
 // Configuración de acceso (edita antes de subir)
 const CONFIG = {
-  // Hash SHA-256 del código de acceso (por defecto: "CAMBIAR-CLAVE-ACA")
-  accessCodeHash: "2799834aaaaad929dffe250910787e1aa05b4927f319f7c5571593adab08ad63",
-  // Lista de correos permitidos (vacío = permite cualquier correo con el código)
+  // Hash SHA-256 del código de acceso (clave: PanchoPintoYBelenAcuna)
+  accessCodeHash: "3bdb5332730e51e13685e5f1390cbd86d9634658d3f156689a3d056028a47c70",
+
+  // Lista de correos permitidos
   allowedEmails: [
-    // "asesor@seremi.cl",
-    // "daem@chillan.cl",
-    // "daem@sannicolas.cl"
+    "franciscopinto@liceosannicolas.cl",
+    "franciscoandresp@gmail.com",
+    "belen.acpe@gmail.com",
+    "belenacuna@liceosannicolas.cl"
+        // Aquí puedes agregar más, por ejemplo:
+    // "otrocorreo@ejemplo.com",
+    // "colega@institucion.cl"
   ]
 };
